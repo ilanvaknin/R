8356fef1a8d3fdf827d92636cb442a6ba881a9d2
ouakniny <52452983+ouakniny@users.noreply.github.com>
2020-06-01 08:28:43 +0300

    Add files via upload

---
+++ regex.R
@@ -0,0 +1,64 @@
+animals <- c("cat", "puppy", "Moose", "MONKEY")
+pattern <- "[a-z]"
+str_detect(animals, pattern)
+
+animals <- c("cat", "puppy", "Moose", "MONKEY")
+pattern <- "[A-Z]$"
+str_detect(animals, pattern)
+
+animals <- c("cat", "puppy", "Moose", "MONKEY")
+pattern <- "[a-z]{4,5}"
+str_detect(animals, pattern)
+
+animals <- c("moose", "monkey", "meerkat", "mountain lion")
+pattern <- "mo*"
+pattern <- "mo?"
+pattern <- "mo+"
+pattern <- "moo*"
+str_detect(animals, pattern)
+
+schools <- c("U. Kentucky","Univ New Hampshire","Univ. of Massachusetts","University Georgia","U California","California State University")
+final <- c("University of Kentucky","University of New Hampshire","University of Massachusetts","University of Georgia","University of California","California State University")
+
+r1 <- schools %>% 
+  str_replace("Univ\\.?|U\\.?", "University ") %>% 
+  str_replace("^University of |^University ", "University of ")
+
+identical(r1, final)
+
+r2 <- schools %>% 
+  str_replace("^Univ\\.?\\s|^U\\.?\\s", "University ") %>% 
+  str_replace("^University of |^University ", "University of ")
+
+identical(r2, final)
+
+r3<-schools %>% 
+  str_replace("^Univ\\.\\s|^U\\.\\s", "University") %>% 
+  str_replace("^University of |^University ", "University of ")
+identical(r3, final)
+
+r4<-schools %>% 
+  str_replace("^Univ\\.?\\s|^U\\.?\\s", "University") %>% 
+  str_replace("University ", "University of ")
+identical(r4,final)
+
+problems <- c("5.3", "5,5", "6 1", "5 .11", "5, 12")
+pattern_with_groups <- "^([4-7])[,\\.](\\d*)$"
+str_replace(problems, pattern_with_groups, "\\1'\\2")
+
+problems <- c("5.3", "5,5", "6 1", "5 .11", "5, 12")
+pattern_with_groups <- "^([4-7])[,\\.\\s](\\d*)$"
+str_replace(problems, pattern_with_groups, "\\1'\\2")
+
+yes <- c("5 feet 7inches", "5 7")
+no <- c("5ft 9 inches", "5 ft 9 inches")
+s <- c(yes, no)
+
+converted <- s %>% 
+  str_replace("\\s*(feet|foot|ft)\\s*", "'") %>% 
+  str_replace("\\s*(inches|in|''|\")\\s*", "") %>% 
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") 
+
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+str_detect(converted, pattern)
+
